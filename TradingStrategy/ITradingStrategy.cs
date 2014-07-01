b19595d4486df7c9760d51edd040f8d7358c2c20
aabbcczz <aabbcczz@live.com>
2014-07-01 17:48:23 +0800

    backup

---
+++ TradingStrategy/ITradingStrategy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using StockAnalysis.Share;
+using MetricsDefinition;
+
+namespace TradingStrategy
+{
+    public interface ITradingStrategy
+    {
+        public EventHandler<TradingEventArgs> OnTrade;
+
+        public void Evaluate(Bar bar);
+
+
+    }
+}
---
+++ TradingStrategy/TradingEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradingStrategy
+{
+    public sealed class TradingEventArgs : EventArgs
+    {
+        public Action
+        public TradingEventArgs()
+            : base()
+        {
+        }
+    }
+}
