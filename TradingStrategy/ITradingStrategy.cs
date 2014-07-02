001abe4f194e64a3a0fc868bc7763561e5353301
aabbcczz <aabbcczz@live.com>
2014-07-02 18:35:21 +0800

    backup

--- TradingStrategy/ITradingStrategy.cs
+++ TradingStrategy/ITradingStrategy.cs
@@ -17,6 +17,7 @@ namespace TradingStrategy
         // start a new period. This function is called each time when a new period is started. 
         // After that, system will call Evaluate function for each trading object.
         // And then call EndPeriod to finish evaluating this period.
+        // The value of parameter 'time' will be in ascending order for each call of this function.
         public void StartPeriod(DateTime time);
 
         public void Evaluate(ITradingObject tradeObject, Bar bar);
