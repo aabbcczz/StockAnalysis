f91d2f70733eedefc85edffc3880bf55f6210d07
aabbcczz <aabbcczz@live.com>
2014-12-13 21:43:13 +0800

    update cmd

--- cmd/GenerateStockBlock.cmd
+++ cmd/GenerateStockBlock.cmd
@@ -2,5 +2,9 @@ CALL SetEnvironment.cmd
 
 COPY /Y %TDXROOT%\T0002\hq_cache\block_zs.dat %STOCKROOT%\
 COPY /Y %TDXROOT%\T0002\hq_cache\tdxzs.cfg %STOCKROOT%\
+COPY /Y %TDXROOT%\T0002\hq_cache\tdxhy.cfg %STOCKROOT%\
+
 
 %BINROOT%\ConvertTdxBlockData.exe -z %STOCKROOT%\block_zs.dat -b %STOCKROOT%\tdxzs.cfg -o %STOCKROOT%\StockBlock.csv
+
+%BINROOT%\ConvertTdxBlockData.exe -h %STOCKROOT%\tdxhy.cfg -b %STOCKROOT%\tdxzs.cfg -o %STOCKROOT%\StockBlock_HangYe.csv
