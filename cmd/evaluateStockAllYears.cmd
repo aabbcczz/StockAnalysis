b33e3daf8698262cf10049accbf873b918f76f33
aabbcczz <aabbcczz@live.com>
2015-04-27 20:59:10 +0800

    update test settings

--- cmd/evaluateStockAllYears.cmd
+++ cmd/evaluateStockAllYears.cmd
@@ -1,3 +1,1 @@
-CALL SetEnvironment.cmd
-
-%BINROOT%\EvaluatorCmdClient -t %SETTINGSROOT%\tradingsettings.xml -c %SETTINGSROOT%\strategysettings.xml -s %SETTINGSROOT%\stockdatasettings.xml -o %STOCKROOT%\codefile.txt -v 2 -i 1000000.0 -a 2012-01-01 -b 2015-12-31 -w 300 -n stocktest -p -k %STOCKROOT%\stockblock.csv %*
+EvaluatorCmdClient -t ..\settings\tradingsettings.xml -c ..\settings\strategysettings.xml -s ..\settings\stockdatasettings.xml -o ..\settings\codefile.txt -v 2 -i 1000000.0 -a 2009-01-01 -b 2013-12-31 -w 300 -n stocktest -p %*
