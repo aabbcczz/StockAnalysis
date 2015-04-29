cdf061bc18643e0a3c1e14fc76c3ca61da4cc7f2
aabbcczz <aabbcczz@live.com>
2015-04-29 09:42:35 +0800

    add data statistic project

---
+++ DataStatistic/Options.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using CommandLine;
+using CommandLine.Text;
+
+namespace DataStatistic
+{
+    public sealed class Options
+    {
+        [Option('s', "StockDataSettingsFile", Required = true, HelpText = "The file that contains the stock data settings")]
+        public string StockDataSettingsFile { get; set; }
+
+        [Option('c', "CodeFile", Required = true, HelpText = "The file that contains the code of stocks")]
+        public string CodeFile { get; set; }
+
+        [Option('o', "OutputFile", Required = true, HelpText = "output file for statistical result")]
+        public string OutputFile { get; set; }
+
+        [Option('v', "Verbose", HelpText = "Verbose level, Range: from 0 to 2.", DefaultValue = 0)]
+        public int VerboseLevel { get; set; }
+
+        [Option('a', "StartDate", Required = true, HelpText = "The start date of statistic")]
+        public DateTime StartDate { get; set; }
+
+        [Option('b', "EndDate", Required = true, HelpText = "The end date of statistic")]
+        public DateTime EndDate { get; set; }
+
+        [HelpOption]
+        public string GetUsage()
+        {
+            string usage = HelpText.AutoBuild(this, current => HelpText.DefaultParsingErrorsHandler(this, current));
+            return usage;
+        }
+
+        public void Print(TextWriter writer)
+        {
+            if (VerboseLevel == 2)
+            {
+                writer.WriteLine("Stock data settings file: {0}", StockDataSettingsFile);
+                writer.WriteLine("Code file: {0}", CodeFile);
+                writer.WriteLine("Start date: {0}", StartDate);
+                writer.WriteLine("End date: {0}", EndDate);
+                writer.WriteLine("Output file: {0}", OutputFile);
+            }
+        }
+
+        public void BoundaryCheck()
+        {
+            VerboseLevel = Math.Max(0, VerboseLevel);
+            VerboseLevel = Math.Min(2, VerboseLevel);
+
+            if (StartDate > EndDate)
+            {
+                var temp = EndDate;
+                EndDate = StartDate;
+                StartDate = temp;
+            }
+        }
+    }
+}
