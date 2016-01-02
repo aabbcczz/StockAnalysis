fe07b8107c3a6f42552fbdf5008016b3e0284189
aabbcczz <aabbcczz@live.com>
2016-01-02 12:02:00 +0800

    add RealTrading project

---
+++ RealTrading/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace RealTrading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string decryptedAccount = TdxWrapper.DecryptAccount("CAEZBMBJ");
+
+            Console.WriteLine(decryptedAccount);
+
+            string encryptedAccount = TdxWrapper.EncryptAccount("13003470");
+            Console.WriteLine(encryptedAccount);
+
+            string encryptedAccount1 = TdxWrapper.EncryptAccount("42000042387");
+            Console.WriteLine(encryptedAccount1);
+
+            string decryptedAccount1 = TdxWrapper.DecryptAccount(encryptedAccount1);
+            Console.WriteLine(decryptedAccount1);
+
+            Console.ReadLine();
+
+            return;
+
+     	    //DLL是32位的,因此必须把C#工程生成的目标平台从Any CPU改为X86,才能调用DLL;
+            //必须把Trade.dll等4个DLL复制到Debug和Release工程目录下;
+            //无论用什么语言编程，都必须仔细阅读VC版内的关于DLL导出函数的功能和参数含义说明，不仔细阅读完就提出问题者因时间精力所限，恕不解答。
+
+            StringBuilder ErrInfo=new StringBuilder(256);
+            StringBuilder Result = new StringBuilder(1024*1024);
+
+
+  
+
+
+            TdxWrapper.OpenTdx();//打开通达信
+
+            //登录
+            int ClientID = TdxWrapper.Logon("119.145.12.70", 443, "2.20", 0, "1111", "1111", "1111", string.Empty, ErrInfo);
+
+            //登录第二个帐号
+            //int ClientID2 = Logon("111.111.111.111", 7708, "4.20", 0, "5555555555", "1111", "555", string.Empty, ErrInfo);
+
+
+
+            if (ClientID==-1)
+            {
+                Console.WriteLine(ErrInfo);
+                return;
+            }
+
+            TdxWrapper.SendOrder(ClientID, 0, 0, "A000001", "601988", 2.5f, 100, Result, ErrInfo);//下单
+	    //SendOrder(ClientID2, 0, 0, "A000001", "601988", 2.5f, 100, Result, ErrInfo);//第二个帐号,下单
+            Console.WriteLine("下单结果: {0}", Result);
+
+
+            TdxWrapper.GetQuote(ClientID, "601988", Result, ErrInfo);//查询五档报价
+            if (ErrInfo.ToString() != string.Empty)
+            {
+                Console.WriteLine(ErrInfo.ToString());
+                return;
+            }
+            Console.WriteLine("行情结果: {0}", Result);
+
+
+
+
+
+            TdxWrapper.QueryData(ClientID, 0, Result, ErrInfo);//查询资金
+            if (ErrInfo.ToString()!=string.Empty)
+            {
+                Console.WriteLine(ErrInfo.ToString());
+                return;
+            }
+            Console.WriteLine("查询结果: {0}", Result);
+
+
+	    //批量查询多个证券的五档报价
+	    string[] Zqdm=new string[]{"600030","600031"};
+  	    string[] Results = new string[Zqdm.Length];
+            string[] ErrInfos = new string[Zqdm.Length];
+
+            IntPtr[] ResultPtr = new IntPtr[Zqdm.Length];
+            IntPtr[] ErrInfoPtr = new IntPtr[Zqdm.Length];
+
+            for (int i = 0; i < Zqdm.Length; i++)
+            {
+                ResultPtr[i] = Marshal.AllocHGlobal(1024 * 1024);
+                ErrInfoPtr[i] = Marshal.AllocHGlobal(256);
+            }
+
+
+
+
+            TdxWrapper.GetQuotes(ClientID, Zqdm, Zqdm.Length, ResultPtr, ErrInfoPtr);
+
+            for (int i = 0; i < Zqdm.Length; i++)
+            {
+                Results[i] =Marshal.PtrToStringAnsi(ResultPtr[i]);
+                ErrInfos[i] = Marshal.PtrToStringAnsi(ErrInfoPtr[i]);
+
+                Marshal.FreeHGlobal(ResultPtr[i]);
+                Marshal.FreeHGlobal(ErrInfoPtr[i]);
+            }
+
+
+
+
+
+
+            TdxWrapper.Logoff(ClientID);//注销
+            TdxWrapper.CloseTdx();//关闭通达信
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RealTrading/TdxWrapper.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace RealTrading
+{
+    public sealed class TdxWrapper
+    {
+        ///基本版
+        [DllImport("trade.dll", CharSet = CharSet.Ansi)]
+        public static extern void OpenTdx();
+        [DllImport("trade.dll", CharSet = CharSet.Ansi)]
+        public static extern void CloseTdx();
+        [DllImport("trade.dll", CharSet = CharSet.Ansi)]
+        public static extern int Logon(string IP, short Port, string Version, short YybID, string AccountNo, string TradeAccount, string JyPassword, string TxPassword, StringBuilder ErrInfo);
+        [DllImport("trade.dll", CharSet = CharSet.Ansi)]
+        public static extern void Logoff(int ClientID);
+        [DllImport("trade.dll", CharSet = CharSet.Ansi)]
+        public static extern void QueryData(int ClientID, int Category, StringBuilder Result, StringBuilder ErrInfo);
+        [DllImport("trade.dll", CharSet = CharSet.Ansi)]
+        public static extern void SendOrder(int ClientID, int Category, int PriceType, string Gddm, string Zqdm, float Price, int Quantity, StringBuilder Result, StringBuilder ErrInfo);
+        [DllImport("trade.dll", CharSet = CharSet.Ansi)]
+        public static extern void CancelOrder(int ClientID, string ExchangeID, string hth, StringBuilder Result, StringBuilder ErrInfo);
+        [DllImport("trade.dll", CharSet = CharSet.Ansi)]
+        public static extern void GetQuote(int ClientID, string Zqdm, StringBuilder Result, StringBuilder ErrInfo);
+        [DllImport("trade.dll", CharSet = CharSet.Ansi)]
+        public static extern void Repay(int ClientID, string Amount, StringBuilder Result, StringBuilder ErrInfo);
+
+
+
+
+        ///普通批量版
+        [DllImport("trade.dll", CharSet = CharSet.Ansi)]
+        public static extern void QueryHistoryData(int ClientID, int Category, string StartDate, string EndDate, StringBuilder Result, StringBuilder ErrInfo);
+        [DllImport("trade.dll", CharSet = CharSet.Ansi)]
+        public static extern void QueryDatas(int ClientID, int[] Category, int Count, IntPtr[] Result, IntPtr[] ErrInfo);
+        [DllImport("trade.dll", CharSet = CharSet.Ansi)]
+        public static extern void SendOrders(int ClientID, int[] Category, int[] PriceType, string[] Gddm, string[] Zqdm, float[] Price, int[] Quantity, int Count, IntPtr[] Result, IntPtr[] ErrInfo);
+        [DllImport("trade.dll", CharSet = CharSet.Ansi)]
+        public static extern void CancelOrders(int ClientID, string[] ExchangeID, string[] hth, int Count, IntPtr[] Result, IntPtr[] ErrInfo);
+        [DllImport("trade.dll", CharSet = CharSet.Ansi)]
+        public static extern void GetQuotes(int ClientID, string[] Zqdm, int Count, IntPtr[] Result, IntPtr[] ErrInfo);
+
+
+        ///高级批量版
+        [DllImport("trade.dll", CharSet = CharSet.Ansi)]
+        public static extern void QueryMultiAccountsDatas(int[] ClientID, int[] Category, int Count, IntPtr[] Result, IntPtr[] ErrInfo);
+        [DllImport("trade.dll", CharSet = CharSet.Ansi)]
+        public static extern void SendMultiAccountsOrders(int[] ClientID, int[] Category, int[] PriceType, string[] Gddm, string[] Zqdm, float[] Price, int[] Quantity, int Count, IntPtr[] Result, IntPtr[] ErrInfo);
+        [DllImport("trade.dll", CharSet = CharSet.Ansi)]
+        public static extern void CancelMultiAccountsOrders(int[] ClientID, string[] ExchangeID, string[] hth, int Count, IntPtr[] Result, IntPtr[] ErrInfo);
+        [DllImport("trade.dll", CharSet = CharSet.Ansi)]
+        public static extern void GetMultiAccountsQuotes(int[] ClientID, string[] Zqdm, int Count, IntPtr[] Result, IntPtr[] ErrInfo);
+
+
+        public static string DecryptAccount(string encryptedAccount)
+        {
+            int len = encryptedAccount.Length;
+
+            if (len % 2 != 0)
+            {
+                throw new ArgumentException("encryptedAccount");
+            }
+
+            len /= 2;
+
+            ushort[] temp = new ushort[len];
+
+            for (int i = 0; i < len; ++i)
+            {
+	            temp[i] = (ushort)((encryptedAccount[2 * i] - 'A') * 26 + (encryptedAccount[2*i + 1] - 'A'));
+            }
+
+            ushort key = 0x55E;
+            for (int i = 0; i < len; ++i)
+            {
+	            ushort v = temp[i];
+	
+	            temp[i] = (ushort)((key >> 8) ^ v);
+	            key = (ushort)((((uint)v + (uint)key) * 0x5207F + 0x6adc3) & 0xFFFF);
+            }
+
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < temp.Length; ++i)
+            {
+                builder.Append((char)temp[i]);
+            }
+
+            return builder.ToString();
+        }
+
+        public static string EncryptAccount(string account)
+        {
+            if (string.IsNullOrEmpty(account))
+            {
+                throw new ArgumentNullException("account");
+            }
+
+            int len = account.Length;
+
+            // get the characters in odd pos
+            len /= 2;
+
+            string oddPosAccount = string.Empty;
+
+            for (int i = 0; i < len; ++i)
+            {
+                oddPosAccount += account[2 * i];
+            }
+            
+            if (account.Length % 2 != 0)
+            {
+                ++len;
+                oddPosAccount += account[2 * len - 2];
+            }
+
+            ushort[] temp = new ushort[len];
+
+            ushort key = 0x55E;
+            for (int i = 0; i < len; ++i)
+            {
+                ushort v = (ushort)oddPosAccount[i];
+
+                temp[i] = (ushort)((key >> 8) ^ v);
+                key = (ushort)((((uint)temp[i] + (uint)key) * 0x5207F + 0x6adc3) & 0xFFFF);
+            }
+
+            byte[] encryptedAccount = new byte[len * 2];
+
+            for (int i = 0; i < len; ++i)
+            {
+                byte a = (byte)(temp[i] / 26);
+                byte b = (byte)(temp[i] % 26);
+
+                encryptedAccount[2 * i] = (byte)(a + 'A');
+                encryptedAccount[2 * i + 1] = (byte)(b + 'A');
+            }
+
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < encryptedAccount.Length; ++i)
+            {
+                builder.Append((char)encryptedAccount[i]);
+            }
+
+            return builder.ToString();
+        }
+    }
+}
